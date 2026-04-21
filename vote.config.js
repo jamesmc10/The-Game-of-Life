b78cbc18e34aa50967d0f675ed78c50e4cd57615
jamesmc10 <jamesmccahill10@gmail.com>
2026-04-21 10:07:49 +0100

    Update vote.config.js

--- vote.config.js
+++ vote.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://jamesmc10.github.io/The-Game-of-Life/',
-})
+  base: "/The-Game-of-Life/",
+});
